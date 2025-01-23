9f29fb4274e18e5f2f5aceb78eb853a51eec1bfc
Priyanka Vithani <priyankavithani@gmail.com>
2025-01-23 16:28:07 -0500

    Lab 4 Auto Layout

---
+++ Priyanka_Vithani_8875646_Lab4/Priyanka_Vithani_8875646_Lab4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lab4
+//
+//  Created by Priyanka Vithani on 28/09/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
